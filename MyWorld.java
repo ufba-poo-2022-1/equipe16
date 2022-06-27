13fe11c82908af592188dc493a13ae6ba472dd75
Diego Anjos <diegoanjos@gmail.com>
2022-06-27 20:57:42 -0300

    updates

--- MyWorld.java
+++ MyWorld.java
@@ -14,8 +14,7 @@ public class MyWorld extends World
      * 
      */
     public MyWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+    {   
         super(700, 700, 1);
         addObject(new Comida(), Greenfoot.getRandomNumber(getWidth()), Greenfoot.getRandomNumber(getHeight()));
         addObject(new Comida(), Greenfoot.getRandomNumber(getWidth()), Greenfoot.getRandomNumber(getHeight()));
